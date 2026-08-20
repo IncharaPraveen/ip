49967149c23001d2f05a90924613714310698097
Inchara <incharap07@gmail.com>
2026-08-20 09:01:45 +0800

    Level-0

---
+++ src/main/java/Computa.java
@@ -0,0 +1,6 @@
+public class Computa {
+    public static void main(String[] args) {
+        String banner = "haiii i am computa! lmk what u need ehaha";
+        System.out.println(banner);
+    }
+}
