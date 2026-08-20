9567b784c172c0c68966c95776a3304f665d639b
Inchara <incharap07@gmail.com>
2026-08-20 14:43:28 +0800

    Level 1

--- src/main/java/Computa.java
+++ src/main/java/Computa.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Computa {
     public static void main(String[] args) {
         String banner = "haiii i am computa! lmk what u need ehaha";
         System.out.println(banner);
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while(!command.equals("bye")){
+            System.out.println(command);
+            command = scanner.nextLine();
+        }
+        System.out.println("Bai Bai! How much wood could a wood chuck chuck..");
+        scanner.close();
+
     }
 }
